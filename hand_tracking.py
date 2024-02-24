c2a3ebb046e876b781f5683e99bc3a3d2d1b2ad6
JignyasAnand <jignyas.ml@gmail.com>
2024-02-24 12:58:08 +0530

    Enable only right hand to draw

---
+++ hand_tracking.py
@@ -0,0 +1,71 @@
+import cv2
+import mediapipe as mp
+from google.protobuf.json_format import MessageToDict
+
+
+class HandTracker:
+    def __init__(self, min_detection_confidence = 0.7, min_tracking_confidence = 0.7, max_num_hands = 2):
+        self.mp_hands = mp.solutions.hands
+        self.drawing = mp.solutions.drawing_utils
+
+        self.hands = self.mp_hands.Hands(
+            model_complexity = 1,
+            min_detection_confidence = min_detection_confidence,
+            min_tracking_confidence = min_tracking_confidence,
+            max_num_hands = max_num_hands
+        )
+        self.lms = None
+        self.processed_img = None
+        self.finger_tip_id = [4, 8, 12, 16, 20]
+
+
+    def track_hands(self, img, left, right, draw = False):
+        flipped = cv2.flip(img, 1)
+        img_rgb = cv2.cvtColor(flipped, cv2.COLOR_BGR2RGB)
+        img = cv2.flip(img, 1)
+        self.processed_img = self.hands.process(img_rgb)
+        mh_lms = self.processed_img.multi_hand_landmarks
+        self.lms = dict()
+
+        if mh_lms:
+            hand_classes = self.processed_img.multi_handedness
+            if len(hand_classes)==2:
+                self.lms["right"] = self.finger_positions(mh_lms[0])
+                self.lms["left"] = self.finger_positions(mh_lms[1])
+                self.draw_landmarks([mh_lms[0]], right)
+                self.draw_landmarks([mh_lms[1]], left)
+            else:
+                if MessageToDict(hand_classes[0])["classification"][0]["label"]=="Right":
+                    self.draw_landmarks([mh_lms[0]], right)
+                    self.lms["right"] = self.finger_positions(mh_lms[0])
+                    self.lms["left"] = None
+                else:
+                    self.draw_landmarks([mh_lms[0]], left)
+                    self.lms["left"] = self.finger_positions(mh_lms[0])
+                    self.lms["right"] = None
+        return self.lms, img, left, right
+
+    def draw_landmarks(self, landmarks, img):
+        for i in landmarks:
+            self.drawing.draw_landmarks(img, i, self.mp_hands.HAND_CONNECTIONS)
+        return img
+
+    def finger_positions(self, x):
+        ret = []
+        if x:
+            myhand = x
+            for ind, lm in enumerate(myhand.landmark):
+                ret.append([ind, lm.x, lm.y])
+        return ret
+
+
+    def find_mode(self):
+        fingers=[0,0,0,0,0]
+        if self.lms and self.lms["right"]:
+            # print(self.lms["right"])
+            if (self.lms["right"][self.finger_tip_id[0]][1] < self.lms["right"][8][1]):
+                fingers[0] = 1
+            for i in range(1,5):
+                if (self.lms["right"][self.finger_tip_id[i]][2] < self.lms["right"][self.finger_tip_id[i] - 2][2]):
+                    fingers[i] = 1
+        return fingers
--- hands.py
+++ hands.py
@@ -3,32 +3,21 @@ import mediapipe as mp
 import numpy as np
 import time
 import screeninfo
-from google.protobuf.json_format import MessageToDict
+import hand_tracking as htm
 
 
-def draw_landmarks(landmarks, img):
-    for i in landmarks:
-        drawing.draw_landmarks(img, i, mp_hands.HAND_CONNECTIONS)
 
 live = cv2.VideoCapture(0)
 
 screen = screeninfo.get_monitors()[0]
 screen_width, screen_height = screen.width, screen.height
 
-mp_hands = mp.solutions.hands
-drawing = mp.solutions.drawing_utils
-
-
-hands = mp_hands.Hands(
-    model_complexity = 1,
-    min_detection_confidence = 0.7,
-    min_tracking_confidence = 0.7,
-    max_num_hands = 2
-)
+tracker = htm.HandTracker()
 
 cv2.namedWindow("Right", cv2.WINDOW_NORMAL)
 cv2.moveWindow("Right", screen_width//2, 0)
 cv2.setWindowProperty("Right", cv2.WND_PROP_TOPMOST, 1)
+
 
 cv2.namedWindow("Left", cv2.WINDOW_NORMAL)
 # cv2.moveWindow("Right", screen_width//2, 0)
@@ -37,6 +26,9 @@ prev_frame_time = 0
 
 # used to record the time at which we processed current frame
 new_frame_time = 0
+image_canvas = None
+xp =0
+yp=0
 
 while True:
     ret, img = live.read(0)
@@ -44,49 +36,64 @@ while True:
         print("ERROR/ FINISHED. Exiting...")
         break
 
-    flipped = cv2.flip(img, 1)
-    img_rgb = cv2.cvtColor(flipped, cv2.COLOR_BGR2RGB)
 
+    left = np.zeros_like(img)
+    # right = np.zeros_like(img)
+    right = cv2.flip(img, 1)
+    if image_canvas is None:
+        image_canvas = np.zeros_like(right)
 
-
-    processed_img = hands.process(img_rgb)
-
-    mh_lms = processed_img.multi_hand_landmarks
-    img = cv2.flip(img, 1)
-
-    blank1 = np.zeros_like(img)
-    blank2 = np.zeros_like(img)
-
-    if mh_lms:
-        # draw_landmarks(mh_lms, img)
-        hand_classes = processed_img.multi_handedness
-        print(len(mh_lms))
-        if len(hand_classes)==2:
-            draw_landmarks([mh_lms[0]], blank1)
-            draw_landmarks([mh_lms[1]], blank2)
+    lms, img, left, right = tracker.track_hands(img, left, right, draw=True)
+    # print(lms)
+    right_mode = "None"
+    if "right" in lms:
+        # lms = lms["right"]
+        fingers = tracker.find_mode()
+        if sum(fingers)==1 and fingers[1]:
+            right_mode = "Drawing mode"
+        elif sum(fingers)==2 and fingers[1]&fingers[2]:
+            right_mode = "Eraser"
         else:
-            if MessageToDict(hand_classes[0])["classification"][0]["label"]=="Right":
-                draw_landmarks([mh_lms[0]], blank1)
-            else:
-                draw_landmarks([mh_lms[0]], blank2)
-        # print(processed_img.multi_handedness)
-        print("=="*10)
+            right_mode = "Idle mode"
 
     new_frame_time = time.time()
     fps = 1/(new_frame_time-prev_frame_time)
     prev_frame_time = new_frame_time
     fps = int(fps)
 
-    cv2.putText(blank1, f"{fps}", (7, 300), cv2.FONT_HERSHEY_SIMPLEX, 3, (100, 255, 0), 3, cv2.LINE_AA)
-    cv2.putText(blank2, f"{fps}", (7, 300), cv2.FONT_HERSHEY_SIMPLEX, 3, (100, 255, 0), 3, cv2.LINE_AA)
+    cv2.putText(left, f"{fps}", (7, 300), cv2.FONT_HERSHEY_SIMPLEX, 3, (100, 255, 0), 3, cv2.LINE_AA)
+    cv2.putText(right, f"{fps} {right_mode}", (7, 300), cv2.FONT_HERSHEY_SIMPLEX, 3, (100, 255, 0), 3, cv2.LINE_AA)
 
 
     screen_height, screen_width, _ = img.shape
 
-    blank1 = cv2.resize(blank1, (screen_width//2, screen_height))
-    blank2 = cv2.resize(blank2, (screen_width//2, screen_height))
+    left = cv2.resize(left, (screen_width//2, screen_height))
+    right = cv2.resize(right, (screen_width//2, screen_height))
+    image_canvas = cv2.resize(image_canvas, (screen_width//2, screen_height))
 
-    resized_height, resized_width, _ = blank1.shape
+    # x, y = 0,0
+    if len(lms)>0 and lms["right"]:
+        x, y = lms["right"][8][1:]
+        x = int(x * (screen_width//2))
+        y = int(y* (screen_height))
+        if right_mode=="Drawing mode":
+            cv2.circle(right, (x, y), 13, (255, 0, 255), cv2.FILLED)
+            cv2.line(right, (xp,yp),(x,y),color= (255, 0, 255), thickness=6)
+            cv2.line(image_canvas, (xp,yp),(x,y),color= (255, 0, 255), thickness=6)
+        elif right_mode=="Eraser":
+            cv2.circle(right, (x, y), 13, (0, 0, 0), cv2.FILLED)
+            cv2.line(right, (xp,yp),(x,y),color= (0, 0, 0), thickness = 20)
+            cv2.line(image_canvas, (xp,yp),(x,y),color= (0, 0, 0), thickness = 20)
+
+        xp , yp = x, y
+
+    img_gray = cv2.cvtColor(image_canvas, cv2.COLOR_BGR2GRAY)
+    _, imginv= cv2.threshold(img_gray, 50, 255, cv2.THRESH_BINARY_INV)
+    imginv = cv2.cvtColor(imginv, cv2.COLOR_GRAY2BGR)
+    right = cv2.bitwise_and(right, imginv)
+    right =cv2.bitwise_or(right, image_canvas)
+
+    resized_height, resized_width, _ = left.shape
     aspect_ratio = resized_width / resized_height
 
     window_width = int(screen_height * aspect_ratio)
@@ -97,8 +104,8 @@ while True:
     cv2.resizeWindow("Left", window_width, screen_height)
     cv2.resizeWindow("Right", window_width, screen_height)
 
-    cv2.imshow("Left", blank1)
-    cv2.imshow("Right", blank2)
+    cv2.imshow("Left", left)
+    cv2.imshow("Right", right)
 
 
     if cv2.waitKey(10)==27:
